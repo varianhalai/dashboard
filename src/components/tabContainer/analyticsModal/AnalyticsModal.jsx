13969f89818aefa5e2ae6a9942d65c52d279bbf0
varianhalai <bastardsinspace@gmail.com>
2021-01-08 20:50:28 -0600

    Added link to address history and moved styling into its own file.

--- src/components/tabContainer/analyticsModal/AnalyticsModal.jsx
+++ src/components/tabContainer/analyticsModal/AnalyticsModal.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import HarvestContext from "../../../Context/HarvestContext";
-import styled from "styled-components";
-import { font } from "../../../styles/appStyles";
+import { Modal, CloseIcon } from "./AnalyticsModalStyles";
 
 const AnalyticsModal = ({ showAnalytics, setShowAnalytics }) => {
   const { state } = useContext(HarvestContext);
@@ -27,76 +26,17 @@ const AnalyticsModal = ({ showAnalytics, setShowAnalytics }) => {
       >
         GRAIN analytics
       </a>
+
+      <a
+        className="analyti-link address-history"
+        href="https://farmdashboard.xyz/history/0x"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Address history
+      </a>
     </Modal>
   );
 };
 
 export default AnalyticsModal;
-
-const Modal = styled.div`
-  &.show {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: ${(state) => state.theme.style.mainBorder};
-    background-color: ${(state) => state.theme.style.highlight};
-    position: absolute;
-    z-index: 500;
-    width: max-content;
-    left: 8rem;
-    top: -0.8rem;
-    border-radius: 0.8rem;
-    padding: 1.5rem;
-    transition: all 0.3s ease-out;
-
-    .analyti-link {
-      font-size: 1.9rem;
-      position: relative;
-      background-color: ${(state) => state.theme.style.wikiTabBackground};
-      border-radius: 0.8rem;
-      margin: 1.5rem 0;
-      padding: 1rem;
-      transition: all 0.25s ease;
-      &.first {
-        @media (max-width: 768px) {
-          top: 2rem;
-        }
-      }
-
-      &:hover {
-        transform: scale(1.05);
-        border: ${(state) => state.theme.style.mainBorder};
-      }
-    }
-
-    @media (max-width: 500px) {
-      left: 0;
-      right: 8rem;
-      margin: auto;
-    }
-  }
-
-  &.hidden {
-    display: none;
-    transition: all 0.3s ease-out;
-  }
-`;
-
-const CloseIcon = styled.span`
-  position: absolute;
-  right: 2rem;
-  top: 2rem;
-  font-size: 1rem;
-  cursor: pointer;
-  color: ${(props) => props.theme.style.buttonFontColor};
-
-  .fas {
-    position: relative;
-    &:hover {
-      top: 1.5px;
-    }
-  }
-  @media (min-width: 768px) {
-    display: none;
-  }
-`;
