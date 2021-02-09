9c0da347b30f05e32e90949ccb08db67e56c1dfe
belbix <7453635@gmail.com>
2021-02-09 03:23:08 +0300

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,18 @@
 
 DEPLOY_CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DEPLOY_CURRENT_COMMIT=$(git rev-parse HEAD)
+
+git checkout main && \
+git fetch origin && \
+yarn build && \
+
 git checkout -b tmp-gh-pages && \
 git add -f ./build && \
 git commit -m "$DEPLOY_CURRENT_BRANCH @ $DEPLOY_CURRENT_COMMIT" && \
 git subtree split --prefix build -b gh-pages && \
 git push -f origin gh-pages:gh-pages && \
-git checkout $DEPLOY_CURRENT_BRANCH
+git checkout $DEPLOY_CURRENT_BRANCH && \
 
 # try this regardless
-git branch -D tmp-gh-pages
+git branch -D tmp-gh-pages && \
 git branch -D gh-pages
